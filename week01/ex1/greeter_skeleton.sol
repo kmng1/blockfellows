866660cc0275706b98049e30df8029f8d004ca9f
Kai Meng <kmng@ieee.org>
2018-06-29 17:02:26 +0800

    Greeting basic solution

--- week01/ex1/greeter_skeleton.sol
+++ week01/ex1/greeter_skeleton.sol
@@ -1,18 +1,29 @@
-pragma solidity ^0.4.16;
+pragma solidity ^0.4.22;
 
 
 contract Greeter {
     /* Add one variable to hold our greeting */
-    string greeting;
+    string public greeting;
 
-    function Greeter(string _greeting) public {
+    constructor(string _greeting) public {
         /* Write one line of code for the contract to set our greeting */
+        greeting = _greeting;
     }
 
-    function greet() constant returns (string)  {
+    function greet() public constant returns (string) {
         /* Write one line of code to allow the contract to return our greeting */
+        return greeting;
+
+    }
+
+    function setGreeting(string newGreeting) public {
+        // change greeting
+        greeting = newGreeting;
     }
 
     /* Add a fallback function to prevent contract payability and non-existent function calls */
+    function () public {
+        revert();
+    }
 
 }
